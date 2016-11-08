e43fec13fb2dda55831650a4b0caf7df1a5f1dbf
techntv <techntvz@gmail.com>
2016-11-08 14:46:12 +0700

    Add modal to choose x or o

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
-"use strict";
-var i;
-var tableContent = document.getElementsByTagName('td');
+(function(){
+  "use strict";
 
-for (i = tableContent.length ; i--;) {
-  tableContent[i].textContent = '';
-}
+  var tableContent = document.getElementsByTagName('td');
+  var modalHTML = "<form class='modalcontent'>";
+  modalHTML += "<h2>Choose sign for player</h2>";
+  modalHTML += "<input type='checkbox' id='xplayer'><span>Choose X</span>";
+  modalHTML += "<input type='checkbox' id='oplayer'><span>Choose O</span>";
+  modalHTML += "</form>";
+
+  window.onload = function (){
+    var modalGame = document.createElement('div');
+    var container = document.getElementsByClassName('container')[0];
+
+    document.body.insertBefore(modalGame, container.nextSibling);
+    modalGame.style.display = "none";
+    modalGame.setAttribute('id', 'modal');
+    modalGame.innerHTML = modalHTML;
+  }
+
+
+
+  function handleNewGame(){
+    var i;
+    var choosePlay = document.getElementById('modal');
+
+    // Clear board game
+    for (i = tableContent.length ; i--;) {
+      tableContent[i].textContent = '';
+    }
+
+    // Choose X or O
+    choosePlay.style.display = 'block';
+
+
+  }
+
+  document.getElementById('btn').addEventListener('click', handleNewGame);
+
+
+
+
+
+}());
