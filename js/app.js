9d4b55d9eed6df935d9965bdee1bb66e4fce5164
techntv <techntvz@gmail.com>
2016-11-09 09:02:15 +0700

    Add option choose player and signer

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 (function(){
   "use strict";
-
   var tableContent = document.getElementsByTagName('td');
+  var player,signer;
 
   var modalPlayerHTML = "<form class='modalPlayer'>";
   modalPlayerHTML += "<h2>How do you want to play ? </h2>";
-  modalPlayerHTML += "<div class='form-item' id='oneplayer'>One Player</div>";
-  modalPlayerHTML += "<div class='form-item' id='twoplayer'>Two Player</div>";
+  modalPlayerHTML += "<div class='form-item player'>One Player</div>";
+  modalPlayerHTML += "<div class='form-item player'>Two Player</div>";
   modalPlayerHTML += "</form>";
 
-
+  var modalSignHTML = "<form class='modalSigner'>";
+  modalSignHTML += "<h2>Would you like to choose X or O ?</h2>";
+  modalSignHTML += "<div class='form-item signer'>X</div>";
+  modalSignHTML += "<div class='form-item signer'>O</div>";
+  modalSignHTML += "</form>";
 
   window.onload = function (){
     var modalPlayer = document.createElement('div');
@@ -23,30 +27,61 @@
     modalPlayer.setAttribute('id', 'modal');
     modalPlayer.innerHTML = modalPlayerHTML;
 
-
+    modalSign.style.display = "none";
+    modalSign.setAttribute('id', 'modalsign');
+    modalSign.innerHTML = modalSignHTML;
   }
-
-
 
   function handleNewGame(){
     var i;
-    var choosePlay = document.getElementById('modal');
-
-    // Clear board game
-    for (i = tableContent.length ; i--;) {
-      tableContent[i].textContent = '';
-    }
-
+    // Reset content on board
+    resetBoard();
     // Choose X or O
-    choosePlay.style.display = 'block';
-
-
+    optionsBoard();
   }
 
+  function playGame(){
+    
+  }
+
+function resetBoard(){
+  var i;
+  for (i = tableContent.length ; i--;) {
+    tableContent[i].textContent = '';
+  }
+}
+
+function optionsBoard(){
+
+  var player = document.getElementsByClassName('player'),
+      signer = document.getElementsByClassName('signer'),
+      choosePlay = document.getElementById('modal'),
+      chooseSign = document.getElementById('modalsign'),
+      i;
+
+      function getPlayer(e){
+        var target = e.target;
+        target.textContent === 'One Player' ? player = 'One Player' : player = 'Two Player';
+        console.log(player);
+        choosePlay.style.display = 'none';
+        chooseSign.style.display = 'block';
+      }
+
+      function getSigner(e){
+        var target = e.target;
+        target.textContent === 'X' ? signer = 'X' : signer = 'O';
+        console.log(signer);
+        chooseSign.style.display = 'none';
+      }
+
+  choosePlay.style.display = 'block';
+  for (i = player.length; i--;) {
+    player[i].addEventListener('click', getPlayer);
+  }
+  for (i = signer.length; i--;) {
+    signer[i].addEventListener('click', getSigner);
+  }
+
+}
   document.getElementById('btn').addEventListener('click', handleNewGame);
-
-
-
-
-
 }());
